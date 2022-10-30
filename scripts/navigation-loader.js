31b79937e472835a2053e8b8df8e10ccf53eadbf
Monkopedia <Monkopedia@users.noreply.github.com>
2022-10-30 16:02:32 +0000

    deploy: 722fe6bb0022b3821fd22df0d511e5a3efa7f735

--- scripts/navigation-loader.js
+++ scripts/navigation-loader.js
@@ -1,31 +1,31 @@
-window.addEventListener('load', () => {
-    fetch(pathToRoot + "navigation.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("sideMenu").innerHTML = data;
-        }).then(() => {
+navigationPageText = fetch(pathToRoot + "navigation.html").then(response => response.text())
+
+displayNavigationFromPage = () => {
+    navigationPageText.then(data => {
+        document.getElementById("sideMenu").innerHTML = data;
+    }).then(() => {
         document.querySelectorAll(".overview > a").forEach(link => {
             link.setAttribute("href", pathToRoot + link.getAttribute("href"));
         })
     }).then(() => {
         document.querySelectorAll(".sideMenuPart").forEach(nav => {
-            if (!nav.classList.contains("hidden")) nav.classList.add("hidden")
+            if (!nav.classList.contains("hidden"))
+                nav.classList.add("hidden")
         })
     }).then(() => {
         revealNavigationForCurrentPage()
+    }).then(() => {
+        scrollNavigationToSelectedElement()
     })
-
-    /* Smooth scrolling support for going to the top of the page */
     document.querySelectorAll('.footer a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
-
             document.querySelector(this.getAttribute('href')).scrollIntoView({
                 behavior: 'smooth'
             });
         });
     });
-})
+}
 
 revealNavigationForCurrentPage = () => {
     let pageId = document.getElementById("content").attributes["pageIds"].value.toString();
@@ -35,9 +35,9 @@ revealNavigationForCurrentPage = () => {
         parts.forEach(part => {
             if (part.attributes['pageId'].value.indexOf(pageId) !== -1 && found === 0) {
                 found = 1;
-                if (part.classList.contains("hidden")){
-                 part.classList.remove("hidden");
-                 part.setAttribute('data-active',"");
+                if (part.classList.contains("hidden")) {
+                    part.classList.remove("hidden");
+                    part.setAttribute('data-active', "");
                 }
                 revealParents(part)
             }
@@ -45,10 +45,47 @@ revealNavigationForCurrentPage = () => {
         pageId = pageId.substring(0, pageId.lastIndexOf("/"))
     } while (pageId.indexOf("/") !== -1 && found === 0)
 };
-
 revealParents = (part) => {
     if (part.classList.contains("sideMenuPart")) {
-        if (part.classList.contains("hidden")) part.classList.remove("hidden");
+        if (part.classList.contains("hidden"))
+            part.classList.remove("hidden");
         revealParents(part.parentNode)
     }
 };
+
+scrollNavigationToSelectedElement = () => {
+    let selectedElement = document.querySelector('div.sideMenuPart[data-active]')
+    if (selectedElement == null) { // nothing selected, probably just the main page opened
+        return
+    }
+
+    let hasIcon = selectedElement.querySelectorAll(":scope > div.overview span.nav-icon").length > 0
+
+    // for instance enums also have children and are expandable, but are not package/module elements
+    let isPackageElement = selectedElement.children.length > 1 && !hasIcon
+    if (isPackageElement) {
+        // if package is selected or linked, it makes sense to align it to top
+        // so that you can see all the members it contains
+        selectedElement.scrollIntoView(true)
+    } else {
+        // if a member within a package is linked, it makes sense to center it since it,
+        // this should make it easier to look at surrounding members
+        selectedElement.scrollIntoView({
+            behavior: 'auto',
+            block: 'center',
+            inline: 'center'
+        })
+    }
+}
+
+/*
+    This is a work-around for safari being IE of our times.
+    It doesn't fire a DOMContentLoaded, presumabely because eventListener is added after it wants to do it
+*/
+if (document.readyState == 'loading') {
+    window.addEventListener('DOMContentLoaded', () => {
+        displayNavigationFromPage()
+    })
+} else {
+    displayNavigationFromPage()
+}
