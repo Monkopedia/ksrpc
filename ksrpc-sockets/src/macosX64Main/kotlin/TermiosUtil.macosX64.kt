cff4ec97a1eaf18532f55a9ea1e5ba760d9b36b4
Jason Monk <jason@level.co>
2024-01-31 13:07:42 -0500

    Be a ULong

--- ksrpc-sockets/src/macosX64Main/kotlin/TermiosUtil.macosX64.kt
+++ ksrpc-sockets/src/macosX64Main/kotlin/TermiosUtil.macosX64.kt
@@ -4,6 +4,6 @@ import platform.posix.ICANON
 import platform.posix.termios
 
 actual fun termios.setICanon() {
-    c_lflag = c_lflag and ICANON.inv().toUInt()
+    c_lflag = c_lflag and ICANON.inv().toULong()
 }
 
