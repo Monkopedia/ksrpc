3c304c64fd69d830c3e9357f204bccdac62b6f14
Jason Monk <monkopedia@gmail.com>
2025-03-23 20:36:57 -0400

    Fix formatting error

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import kotlinx.validation.ExperimentalBCVApi
 import org.jetbrains.dokka.base.DokkaBase
 import org.jetbrains.dokka.base.DokkaBaseConfiguration
-import kotlinx.validation.ExperimentalBCVApi
 
 buildscript {
     dependencies {
