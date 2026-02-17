d756738a63d1a9d79c956bc951b90f44a0412d80
Jason Monk <monkopedia@gmail.com>
2026-02-16 19:19:03 -0500

    test(ktor): add module-local coverage for client/server/websocket variants

---
+++ ksrpc-ktor/server/src/jvmTest/kotlin/com/monkopedia/ksrpc/ktor/KtorServerModuleTypeReferenceTest.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2026 Jason Monk <monkopedia@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.monkopedia.ksrpc.ktor
+
+import com.monkopedia.ksrpc.KsrpcEnvironment
+import com.monkopedia.ksrpc.channels.SerializedService
+import io.ktor.server.routing.Routing
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+class KtorServerModuleTypeReferenceTest {
+    @Test
+    fun ktorServerTopLevelFunctionIsReachableWithoutReflection() {
+        val function: Routing.(String, SerializedService<String>, KsrpcEnvironment<String>) -> Unit =
+            ::serve
+
+        assertNotNull(function)
+    }
+}
